e57b2252cf7190593e81ef75eaca86a26f0f26f8
Friedemann Wachsmuth <me@peaceman.de>
2020-10-17 19:02:44 +0200

    restructure DNG converter script

--- convert.py
+++ convert.py
@@ -1,84 +1,104 @@
 """
-Converts raw RPi Cam JPGs to DNGs using PyDNG
+Converts raw RPi Cam JPEGs to DNGs using PyDNG
 
-Help/usage: python convert.py -h
+# Usage
+
+CLI:
+    python3 convert.py -h
+
+Python:
+    import convert
+    convert( <...> )
 """
 
-import os
-import io
 import argparse
+import io
+import os
+import sys
+
 from pydng.core import RPICAM2DNG as RPiCam2DNG
 
-def main():
+def convert(input: str, output: str=None, compress=False, cinema_dng=False, keep_jpgs=False):
+    converter = RPiCam2DNG()
+
+    input = os.path.realpath(input)
+
+    if not os.path.exists(input):
+        raise FileNotFoundError("The input dir " + input + " doesn't exist")
+
+    dir_name = os.path.basename(input)
+    if output:
+        output = os.path.realpath(output)
+    else:
+        output = os.path.join(os.path.dirname(
+            os.path.dirname(input)), 'DNGs', dir_name)
+        if cinema_dng:
+            output = os.path.join(
+                output, 'CONTENTS', 'IMAGE', '000000')
+
+    if not os.path.exists(output):
+        os.makedirs(output)
+    elif cinema_dng:
+        raise FileExistsError(
+            "CinemaDNG '" + os.path.dirname(os.path.dirname(
+                os.path.dirname(output))) + "' already exists")
+
+    if cinema_dng:
+        clip_number = 0
+        dng_number = 0
+
+    no_jpg_error = True
+
+    for file_name in os.listdir(input):
+        if not file_name.lower().endswith('.jpg'):
+            continue
+
+        no_jpg_error = False
+
+        file = os.path.join(input, file_name)
+        if cinema_dng:
+            if dng_number == 9999:
+                clip_number += 1
+                output = output[:-6] + str(clip_number).zfill(4) + '00'
+                if not os.path.exists(output):
+                    os.mkdir(output)
+
+            destination_file = os.path.join(output, dir_name + '_' + str(dng_number).zfill(4) + '.dng')
+            dng_number += 1
+        else:
+            destination_file = os.path.join(output, file_name[:-3] + 'dng')
+
+        with open(file, 'rb') as jpg_file, \
+             open(destination_file, 'wb') as dng_file:
+            print(file + "; " + destination_file)
+            dng_file.write(converter.convert(io.BytesIO(jpg_file.read()), compress=compress))
+
+        if not keep_jpgs:
+            os.remove(file)
+
+    if no_jpg_error:
+        raise FileNotFoundError("No JPG files found")
+
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="convert RPi Cam JPEGs in a dir to DNGs")
     parser.add_argument(
         '-i', '--input', default=os.getcwd(), type=str,
         help="(default: .)", metavar='<input dir>')
     parser.add_argument(
-        '-o', '--output', type=str, help="(default: "
-        + os.path.join('..', '..', 'DNGs', '') + '<current dir name>)',
+        '-o', '--output', type=str, help="(default: <input dir"
+        + os.path.join('>', '..', '..', 'DNGs', '<') + 'current dir name>)',
         metavar='<output dir>')
     parser.add_argument(
-        '--compress', help="approx. 33%% smaller files, 288%% as much conversion time", action="store_true")
+        '--compress', help="approx. 33%% smaller files, 288%% as much conversion time",
+        action="store_true")
     parser.add_argument(
         '--cinema-dng', help="generate output in the Adobe CinemaDNG format",
         action='store_true')
     parser.add_argument(
         '--keep-jpgs', help="keep input files", action='store_true')
-
-    converter = RPiCam2DNG()
-
-    args = parser.parse_args()
-
-    images_dir = os.path.realpath(args.input)
-
-    if not os.path.exists(images_dir):
-        raise FileNotFoundError("The input dir " + images_dir + " doesn't exist")
-
-    dir_name = os.path.basename(images_dir)
-    if args.output:
-        destination_dir = os.path.realpath(args.output)
-    else:
-        destination_dir = os.path.join(os.path.dirname(
-            os.path.dirname(images_dir)), 'DNGs', dir_name)
-        if args.cinema_dng:
-            destination_dir = os.path.join(
-                destination_dir, 'CONTENTS', 'IMAGE', '000000')
-
-    if not os.path.exists(destination_dir):
-        os.makedirs(destination_dir)
-    elif args.cinema_dng:
-        raise FileExistsError(
-            "CinemaDNG '" + os.path.dirname(os.path.dirname(
-                os.path.dirname(destination_dir))) + "' already exists")
-
-    if args.cinema_dng:
-        if len(os.listdir(images_dir)) > 9999_9999:
-            raise Exception("The maximum of 9999,9999 DNGs in one CinemaDNG would be exceeded")
-        clip_number = 0
-        dng_number = 0
-
-    for file_name in os.listdir(images_dir):
-        if file_name.lower().endswith('.jpg'):
-            file = os.path.join(images_dir, file_name)
-            if args.cinema_dng:
-                if dng_number == 9999:
-                    clip_number += 1
-                    destination_dir = destination_dir[:-6] + str(clip_number).zfill(4) + '00'
-                    if not os.path.exists(destination_dir):
-                        os.mkdir(destination_dir)
-
-                destination_file = os.path.join(destination_dir, dir_name + '_' + str(dng_number).zfill(4) + '.dng')
-                dng_number += 1
-            else:
-                destination_file = os.path.join(destination_dir, file_name[:-3] + 'dng')
-
-            with open(file, 'rb') as jpg_file, \
-                 open(destination_file, 'wb') as dng_file:
-                dng_file.write(converter.convert(io.BytesIO(jpg_file.read()), compress=args.compress))
-            if not args.keep_jpgs:
-                os.remove(file)
-
-if __name__ == '__main__':
-    main()
+    
+    # Call function convert with the command-line args as keyworded python args
+    convert(**vars(parser.parse_args()))
+else:
+    sys.modules[__name__] = convert
