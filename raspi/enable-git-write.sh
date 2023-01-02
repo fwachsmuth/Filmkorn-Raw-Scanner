dac7851d5cabbdd421a6a3d77b97555ffbaa4cc3
Friedemann Wachsmuth <>
2023-01-02 18:33:24 +0100

    Helper to allow gitwrite on the raspi

---
+++ raspi/enable-git-write.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# This script is called by opme.sh, after pairing. 
+
+cd Filmkorn-Raw-Scanner
+git remote set-url origin git@github.com:fwachsmuth/Filmkorn-Raw-Scanner.git
+#git config --global user.email ""
+#git config --global user.name ""
+if ! grep -q id_filmkorn-scanner-dev_ed25519 ~/.ssh/config; then
+    cat <<EOT >> ~/.ssh/config
+Host github.com
+  HostName github.com
+  IdentityFile ~/.ssh/id_filmkorn-scanner-dev_ed25519
+EOT
+  chmod 600 ~/.ssh/config
+fi
