31ca2c29ea799779a8391c7b98271acb5678b610
Friedemann Wachsmuth <me@peaceman.de>
2024-01-03 13:31:24 +0100

    Fixing bootstrapper's fuse handling

--- bootstrap/flash-atmega328.sh
+++ bootstrap/flash-atmega328.sh
@@ -6,13 +6,15 @@
 # !!!!!!!
 
 # writing Fuses
+# Note that 0x05 is the same as 0xFD, because only the bottom three bits are used. 0xFD throws an error though.
 sudo avrdude \
-    -C ~/avrdude_gpio.conf \
+    -C \
     -v \
     -p atmega328p \
     -c pi_1 \
+    -e \
     -U lock:w:0x3F:m \
-    -U efuse:w:0xFD:m \
+    -U efuse:w:0x05:m \
     -U hfuse:w:0xDA:m \
     -U lfuse:w:0xFF:m 
 
@@ -24,3 +26,4 @@ sudo avrdude \
 	-p atmega328p \
 	-c pi_1 \
 	-U flash:w:scan-controller-v1.0.ino.hex:i
+    
