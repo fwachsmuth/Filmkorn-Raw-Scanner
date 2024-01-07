a861e7aca5e2cd1212471cbdd7bbcfdab66d0627
Friedemann Wachsmuth <me@peaceman.de>
2024-01-07 21:06:14 +0100

    adds basic concept of starting python on the mac remotely

--- host-computer/start_converting.sh
+++ host-computer/start_converting.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
+#!/bin/bash --login
 # This script shall be launched on the host computer, not on the Raspi.
 # It starts the watchdog and conversion process to turn Scanner Raw Files to DNG and then CinemaDNG.
 
+# call this from the raspi like 
+# ssh -i ~/.ssh/id_filmkorn-scanner_ed25519 peaceman@wachsmut-mbp-2021.local "cd code/Filmkorn-Raw-Scanner/host-computer; ./start_converting.sh"
+
+cd helper
+python3 cineDNG_creator.py -i "`cat ../.scan_destination`/raw-intermediates/" -o "`cat ../.scan_destination`/CinemaDNG/." --cinema-dng --keep-running
