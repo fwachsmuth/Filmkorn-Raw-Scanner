fe5f664b89a11103af160e4354a61cd57c86e06b
Friedemann Wachsmuth <me@peaceman.de>
2024-01-07 15:46:50 +0100

    More changes to the directory structure

--- host-computer/helper/opme.sh
+++ host-computer/helper/opme.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-scp id_filmkorn-scanner-dev_ed25519* pi@filmkorn-scanner.local:~/.ssh
+scp ~/.ssh/id_filmkorn-scanner-dev_ed25519* pi@filmkorn-scanner.local:~/.ssh
 ssh pi@filmkorn-scanner.local "cd Filmkorn-Raw-Scanner/raspi; ./enable-git-write.sh"
