2b27cfb36126f9cec312d5da4b8449612d7e0aac
Friedemann Wachsmuth <me@peaceman.de>
2024-01-07 15:46:50 +0100

    More changes to the directory structure

---
+++ host-computer/start_converting.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# This script shall be launched on the host computer, not on the Raspi.
+# It starts the watchdog and conversion process to turn Scanner Raw Files to DNG and then CinemaDNG.
+
