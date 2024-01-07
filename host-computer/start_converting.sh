53fa5a18362d6a31297aa95764ff4f4a180e1f9e
Friedemann Wachsmuth <me@peaceman.de>
2024-01-07 23:28:32 +0100

    adding blueprint for smart mac remote control :)

--- host-computer/start_converting.sh
+++ host-computer/start_converting.sh
@@ -3,7 +3,7 @@
 # It starts the watchdog and conversion process to turn Scanner Raw Files to DNG and then CinemaDNG.
 
 # call this from the raspi like 
-# ssh -i ~/.ssh/id_filmkorn-scanner_ed25519 peaceman@wachsmut-mbp-2021.local "cd code/Filmkorn-Raw-Scanner/host-computer; ./start_converting.sh"
+# ssh -i ~/.ssh/id_filmkorn-scanner_ed25519 `cat .user_and_host` "cd `cat .host_path`; ./start_converting.sh"
 
 cd helper
 python3 cineDNG_creator.py -i "`cat ../.scan_destination`/raw-intermediates/" -o "`cat ../.scan_destination`/CinemaDNG/." --cinema-dng --keep-running
