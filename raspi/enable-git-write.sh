fa8188dbe750d397794de552e8fa0c7930ddce4e
Friedemann Wachsmuth <me@peaceman.de>
2024-01-12 01:31:27 +0100

    fixes to enable-git-write.sh

--- raspi/enable-git-write.sh
+++ raspi/enable-git-write.sh
@@ -3,9 +3,9 @@
 
 # This script is called from remote by opme.sh, after pairing. 
 
-cd Filmkorn-Raw-Scanner
+cd ~/Filmkorn-Raw-Scanner
 git remote set-url origin git@github.com:fwachsmuth/Filmkorn-Raw-Scanner.git
-#git config --global user.email ""
+git config --global user.email "me@peaceman.de"
 git config --global user.name "Friedemann Wachsmuth"
 if ! grep -q id_filmkorn-scanner-dev_ed25519 ~/.ssh/config; then
     cat <<EOT >> ~/.ssh/config
