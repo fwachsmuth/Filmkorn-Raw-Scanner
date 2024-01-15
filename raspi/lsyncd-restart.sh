14997417fe75066be2746181fc3509f3bdc67ba0
Friedemann Wachsmuth <me@peaceman.de>
2024-01-16 00:33:23 +0100

    removing unneeded shell script

--- raspi/lsyncd-restart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# to be run on the raspi, not on the host computer!
-# This script usually gets called by update-destinations.sh
-echo "Restarting Killing lsyncd..."
-sudo systemctl restart lsyncd
-sudo systemctl status --no-pager -20 lsyncd
