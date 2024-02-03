48b88ee3963e2073c018def671b4cc46702a6767
Friedemann Wachsmuth <me@peaceman.de>
2024-02-03 13:12:32 +0100

    Don't attempt to set fuses on the raspi for now

--- scan-controller/bootstrap/flash-atmega328.sh
+++ scan-controller/bootstrap/flash-atmega328.sh
@@ -5,17 +5,21 @@
 # !!!!!!!
 
 # writing Fuses
-sudo avrdude \
-    -C ~/Filmkorn-Raw-Scanner/scan-controller/bootstrap/avrdude_gpio.conf \
-    -v \
-    -p atmega328p \
-    -c pi_1 \
-    -e \
-    -U lock:w:0x3F:m \
-    -U efuse:w:0xFD:m \
-    -U hfuse:w:0xDA:m \
-    -U lfuse:w:0xFF:m 
 
+# The below doesn't seem to work when run from a raspi, probably due to programming voltage being too low (~3.2V)
+# It might also be a problem with the older Version 6.3-20171130 running on the raspi. IDE uses 6.3-20190619!
+# Meanwhile, we need to burn the fuses using an external programmer.
+#
+# sudo avrdude \
+#     -C ~/Filmkorn-Raw-Scanner/scan-controller/bootstrap/avrdude_gpio.conf \
+#     -v \
+#     -p atmega328p \
+#     -c pi_1 \
+#     -e \
+#     -U lock:w:0x3F:m \
+#     -U efuse:w:0xFD:m \
+#     -U hfuse:w:0xDA:m \
+#     -U lfuse:w:0xFF:m 
 
 # Burn uC Code & bootloader
 sudo avrdude \
