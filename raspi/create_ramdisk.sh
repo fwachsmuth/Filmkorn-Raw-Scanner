e92f3fb95a56ce9d6c9f7bb8d90f6cb43c1fc812
Friedemann Wachsmuth <me@peaceman.de>
2025-12-22 17:30:28 +0100

    Create ramdisk mountpoint if not existing yet

--- raspi/create_ramdisk.sh
+++ raspi/create_ramdisk.sh
@@ -5,4 +5,5 @@ total_ram=$(grep MemTotal /proc/meminfo | awk '{print $2}')
 ramdisk_size=$((total_ram - 1000000)) # leave 1 GB free
 
 # Create RAM disk
+mkdir -p /mnt/ramdisk
 mount -t tmpfs -o size=${ramdisk_size}k tmpfs /mnt/ramdisk
