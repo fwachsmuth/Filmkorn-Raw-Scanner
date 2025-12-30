fc98be92cda26f9146a700dc432dd7945802206a
Friedemann Wachsmuth <me@peaceman.de>
2025-12-30 20:20:21 +0100

    ad logging to the updater

--- raspi/update.sh
+++ raspi/update.sh
@@ -9,20 +9,34 @@ fi
 
 REPO_DIR="/home/pi/Filmkorn-Raw-Scanner"
 SERVICE_NAME="filmkorn-scanner.service"
+LOG_TAG="filmkorn-update"
+
+log() {
+  logger -t "$LOG_TAG" "$*"
+}
 
 cd "$REPO_DIR"
+log "update: stopping $SERVICE_NAME"
 sudo systemctl stop "$SERVICE_NAME" || true
 cleanup() {
+  log "update: starting $SERVICE_NAME"
   sudo systemctl start "$SERVICE_NAME" || true
 }
 trap cleanup EXIT
+log "update: fetching tags"
 git fetch --tags --prune
+log "update: checking out $TAG"
 git checkout "$TAG"
 
 if [ -f scan-controller/scan-controller.ino.with_bootloader.hex ]; then
-  SKIP_SERVICE_RESTART=1 bash scan-controller/bootstrap/flash-atmega328.sh
+  log "update: flashing controller (avrdude)"
+  SKIP_SERVICE_RESTART=1 bash scan-controller/bootstrap/flash-atmega328.sh 2>&1 \
+    | while IFS= read -r line; do log "avrdude: $line"; done
+  log "update: flashing complete"
 fi
 
+log "update: reloading systemd"
 sudo systemctl daemon-reload || true
 trap - EXIT
+log "update: rebooting"
 sudo reboot
