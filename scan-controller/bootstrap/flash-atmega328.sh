1f800ab9ff62c3e6380a7abf53917e87212bd545
Friedemann Wachsmuth <me@peaceman.de>
2026-01-03 23:06:38 +0100

    check if we really need to stop the service

--- scan-controller/bootstrap/flash-atmega328.sh
+++ scan-controller/bootstrap/flash-atmega328.sh
@@ -4,16 +4,6 @@
 # Make sure to run this on the Raspi, which will directly flash the AtMega328 on the connected controller board.
 # !!!!!!!
 
-# Let's stop the scanner service while we flash the microcontroller to free up the GPIOs and avoid interference
-if [ -z "${SKIP_SERVICE_RESTART:-}" ]; then
-  SERVICE_NAME="filmkorn-scanner.service"
-  sudo systemctl stop "$SERVICE_NAME"
-  cleanup() {
-    sudo systemctl start "$SERVICE_NAME"
-  }
-  trap cleanup EXIT
-fi
-
 # This is how we successfully flash in raspi/dev/ino-update.sh, so we replicate it here:
 # run_and_log "flash" /usr/local/bin/avrdude \
 #   -C /home/pi/Filmkorn-Raw-Scanner/scan-controller/avrdude_gpio.conf \
@@ -39,13 +29,44 @@ fi
 
 
 
-python3 - <<'PY'
-import RPi.GPIO as GPIO
-
-UC_POWER_GPIO = 16  # GPIO16 (physical pin 36) enables µC power switch on the controller PCB
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(UC_POWER_GPIO, GPIO.OUT, initial=GPIO.HIGH)
+# If GPIO16 (uC power) is already high, skip stopping the service and re-driving it.
+gpio16_level() {
+  python3 - <<'PY'
+try:
+    import RPi.GPIO as GPIO
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(16, GPIO.IN)
+    print(1 if GPIO.input(16) else 0)
+except Exception:
+    print(0)
 PY
+}
+
+set_gpio16_high() {
+  python3 - <<'PY'
+try:
+    import RPi.GPIO as GPIO
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(16, GPIO.OUT, initial=GPIO.HIGH)
+except Exception:
+    pass
+PY
+}
+
+GPIO16_LEVEL="$(gpio16_level || true)"
+if [ "$GPIO16_LEVEL" = "1" ]; then
+  SKIP_SERVICE_RESTART=1
+else
+  if [ -z "${SKIP_SERVICE_RESTART:-}" ]; then
+    SERVICE_NAME="filmkorn-scanner.service"
+    sudo systemctl stop "$SERVICE_NAME"
+    cleanup() {
+      sudo systemctl start "$SERVICE_NAME"
+    }
+    trap cleanup EXIT
+  fi
+  set_gpio16_high
+fi
 
 HEX_PATH="/home/pi/Filmkorn-Raw-Scanner/scan-controller/build/arduino.avr.pro/scan-controller.ino.with_bootloader.hex"
 CONF_PATH="/home/pi/Filmkorn-Raw-Scanner/scan-controller/avrdude_gpio.conf"
