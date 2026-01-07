9e0acd8fecd48536a5114a6685a92cb74fe05bf3
Friedemann Wachsmuth <me@peaceman.de>
2026-01-07 22:45:18 +0100

    Fix "enable dev mode" form remote

--- host-computer/helper/dev/install-dev-key.sh
+++ host-computer/helper/dev/install-dev-key.sh
@@ -46,5 +46,8 @@ fi
 info "Copying dev key to scanner..."
 scp ~/.ssh/id_filmkorn-scanner-dev_ed25519* pi@filmkorn-scanner.local:~/.ssh
 
+info "Fixing SSH key permissions on the scanner..."
+ssh pi@filmkorn-scanner.local "chmod 700 ~/.ssh && chmod 600 ~/.ssh/id_filmkorn-scanner-dev_ed25519 && chmod 644 ~/.ssh/id_filmkorn-scanner-dev_ed25519.pub"
+
 info "Enabling git write access on the scanner..."
 ssh pi@filmkorn-scanner.local "cd Filmkorn-Raw-Scanner/raspi/dev; ./enable-git-write.sh"
